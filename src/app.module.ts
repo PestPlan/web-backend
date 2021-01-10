b0c5e6995e4d7196c0548c21524b97f23ca4a01b
baesh3744 <baesh3744@naver.com>
2021-01-10 15:58:31 +0900

    Remove duplicated sequelize module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,15 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoginModule } from './modules/login/login.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { HomeModule } from './modules/home/home.module';
-import { User } from './models/entities/user.entity';
-import { Device } from './models/entities/device.entity';
-import { Notice } from './models/entities/notice.entity';
+import { LoginModule } from './modules/login/login.module';
 
 @Module({
     imports: [
-        LoginModule,
-        SequelizeModule.forRoot({
-            dialect: 'mariadb',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: 'root',
-            database: 'test',
-            models: [User, Device, Notice],
-            define: {
-                timestamps: false,
-            },
-            autoLoadModels: true,
-        }),
         AuthModule,
         HomeModule,
+        LoginModule,
     ],
     controllers: [AppController],
     providers: [AppService],
