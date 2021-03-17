965abf95ca2e5f008277b1b07fab8d9fba0f5f10
donggyu-kim <kimss7334@naver.com>
2021-03-17 17:49:12 +0900

    auto

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
             },
         }),
     );
-    await app.listen(process.env.PORT || 3000);
+    const port: number = parseInt(`${process.env.PORT}`) || 3000;
+    await app.listen(port);
 }
 bootstrap();
