b9e63d889d18f0b5507bbf6314578e7464268c0a
baesh3744 <baesh3744@naver.com>
2021-05-29 17:07:05 +0900

    Import DatabaseModule to update MariaDB

--- src/modules/notices/notices.module.ts
+++ src/modules/notices/notices.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
 import { devicesProviders } from 'src/models/providers/devices.providers';
 import { NoticesController } from './notices.controller';
 import { NoticesService } from './notices.service';
 
 @Module({
+    imports: [DatabaseModule],
     controllers: [NoticesController],
     providers: [NoticesService, ...devicesProviders],
 })
